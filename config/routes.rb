a9b58060d97b24a9f7c3c82f6cd060f877e5b675
Ukaypromise <promiseukaimo@gmail.com>
2023-11-04 03:47:03 +0100

    Setup devise and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
   namespace :api, defaults: {format: :json} do
-    
+    namespace :v1 do
+      
+    end
   end
 end
