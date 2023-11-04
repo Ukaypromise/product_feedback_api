c4932daf1cf3ee80542f87101b7d56483cc306ed
Ukaypromise <promiseukaimo@gmail.com>
2023-11-04 05:28:47 +0100

    Generate custom sessions and registration controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: "api/v1/users",
+                     controllers: {
+                       registrations: "api/v1/users/registrations",
+                       sessions: "api/v1/users/sessions",
+                     }
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      
     end
   end
 end
