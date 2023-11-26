5adc6f4e52ed2e1efb8ee6cc0b87d66123b23cfc
Ukaypromise <promiseukaimo@gmail.com>
2023-11-26 05:06:21 +0100

    Add create and delete for votes

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -9,4 +9,20 @@ class Api::V1::VotesController < ApplicationController
       render json: @vote.errors, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @vote = Vote.find(params[:id])
+    if @vote.user_id != current_user.id
+      render json: { error: "Not authorized" }, status: :unauthorized
+    elsif @vote.destroy
+      render json: { message: "Vote deleted sucessfully.",
+                     data: @vote, status: :ok }
+    else
+      render json: {
+               status: { message: "Vote couldn't be deleted successfully." },
+               errors: @vote.errors.full_messages,
+               status: :unprocessable_entity,
+             }
+    end
+  end
 end
