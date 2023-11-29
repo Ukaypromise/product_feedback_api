e0cc72deb85f5266317fd51942b698b203292ca5
Ukaypromise <promiseukaimo@gmail.com>
2023-11-29 11:33:37 +0100

    Add search filtering

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,15 @@ class Company < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  # scope :search, ->(search_query) {
+  #         search_query = search_query.strip
+  #         where("name ILIKE ?", "%#{search_query}%")
+  #       }
+  scope :search, ->(search_query) {
+          search_query = search_query.strip
+          where("name LIKE :search", search: "%#{search_query}%")
+        }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,11 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :product_url, presence: true
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  scope :search, ->(search_query) {
+          search_query = search_query.strip
+          where("name ILIKE ?", "%#{search_query}%")
+        }
 end
